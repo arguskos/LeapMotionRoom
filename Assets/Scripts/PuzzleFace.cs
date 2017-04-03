efb10699a6edb71e5c4a16a21d360f107c8695cd
arguskos <arguskos79@gmail.com>
2017-04-03 10:45:17 +0200

    start proj

---
+++ Assets/Scripts/PuzzleFace.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+struct Element
+
+{
+    Vector3 Position;
+    bool State;
+    Action  Appearing;
+}
+
+public class PuzzleFace : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
