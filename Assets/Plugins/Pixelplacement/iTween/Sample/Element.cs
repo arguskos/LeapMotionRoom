e92cdf13f4d866f94c92b5ce7af2fec8ccee0a81
arguskos <arguskos79@gmail.com>
2017-04-03 12:31:41 +0200

    iTween Basic

---
+++ Assets/Plugins/Pixelplacement/iTween/Sample/Element.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Element : MonoBehaviour
+{
+
+
+    public Element(GameObject obj)
+    {
+        Position = obj.transform.position;
+        IsOn = false;
+    }
+    private Vector3 Position;
+    private bool IsOn;
+
+    public void OnCompleteOn()
+    {
+        IsOn = true;
+        Debug.Log("off");
+    }
+    public void OnCompleteOff()
+    {
+        IsOn = false;
+        Debug.Log("off");
+    }
+    public void DeffaultOn()
+    {
+       iTween.MoveBy(gameObject, iTween.Hash("x", -0.01f, "easeType", "easeInOutExpo", "loopType", "none", "delay", 0, "oncomplete", "OnCompleteOn", "oncompletetarget", gameObject));
+
+
+    }
+
+    public void DeffaultOff()
+    {
+        iTween.MoveBy(gameObject, iTween.Hash("x", -0.01f, "easeType", "easeInOutExpo", "loopType", "none", "delay", 0, "oncomplete", "OnCompleteOff", "oncompletetarget", gameObject));
+
+
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/PuzzleFace.cs
+++ Assets/Scripts/PuzzleFace.cs
@@ -2,24 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-struct Element
 
+public class PuzzleFace : MonoBehaviour
 {
-    Vector3 Position;
-    bool State;
-    Action  Appearing;
-}
 
-public class PuzzleFace : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    private int _numberOfElements;
+    private List<Element> ElementObjects = new List<Element>();
+
+
+    void Start()
+    {
+
+        foreach (Transform child in transform)
+        {
+
+            ElementObjects.Add(child.gameObject.GetComponent < Element>());
+        }
+        _numberOfElements = ElementObjects.Count;
+        for (int i = 0; i < ElementObjects.Count; i++)
+        {
+            ElementObjects[i].DeffaultOn();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
