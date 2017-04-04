10521ddcd6da41d03eafd92339272dfc34096239
Weraine <arne.wesenbeek@student.kdg.be>
2017-04-04 11:26:54 +0200

    buttonInput
    required code for button input

---
+++ Assets/Scripts/UseBtnTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseBtnTest : MonoBehaviour
+{
+    public ArduinoButtons testje;
+	// Use this for initialization
+	void Start () {
+        //testje = (ArduinoButtons)testje.GetComponent(typeof(ArduinoButtons));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (testje.btn0Pressed)
+        {
+            Debug.Log(testje.btn0Pressed);
+        }
+	}
+}
